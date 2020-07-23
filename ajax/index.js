e62cd231f67af12f961f372687566b489849b3ed
yokoto <yo44ko55@gmail.com>
2020-07-23 15:28:41 +0900

    データを表示する

--- ajax/index.js
+++ ajax/index.js
@@ -10,25 +10,42 @@ function fetchUserInfo(userId) {
       } else {
         return response.json().then(userInfo => {
           console.log(userInfo);
+
+          const view = escapeHTML`
+              <h4>${userInfo.name} (@${userInfo.login})</h4>
+              <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height='100'>
+              <dl>
+                <dt>Location</dt>
+                <dd>${userInfo.location}</dd>
+                <dt>Repositories</dt>
+                <dd>${userInfo.public_repos}</dd>
+              </dl>
+            `;
+          const result = document.getElementById('result');
+          result.innerHTML = view;
         });
       }
     }).catch(error => {
       console.error(error);
     });
-  // XMLHttpRequest
-  // const request = new XMLHttpRequest();
-  // request.open('GET', `https://api.github.com/users/${encodeURIComponent(userId)}`);
-  // request.addEventListener('load', () => {
-  //   if (request.status >= 200 && request.status < 300) {
-  //     const userInfo = JSON.parse(request.responseText);
-  //     console.log(userInfo)
-  //   } else {
-  //     console.error('エラーレスポンス', request.statusText);
-  //   }
-  // });
-  // request.addEventListener('error', () => {
-  //   console.log('ネットワークエラー');
-  // });
-  // request.send();
 }
 
+function escapeSpecialChars(str) {
+  return str
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#039;');
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === 'string') {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
