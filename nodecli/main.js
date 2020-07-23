fc240965864988471ae75888678bdd19c04db753
yokoto <yo44ko55@gmail.com>
2020-07-23 18:44:40 +0900

    ユニットテストを記述する

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,6 +1,6 @@
 const program = require('commander');
 const fs = require('fs');
-const marked = require('marked');
+const md2html = require('./md2html');
 
 program.option('--gfm', 'GFMを有効にする');
 program.parse(process.argv);
@@ -18,8 +18,6 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file, {
-    gfm: cliOptions.gfm
-  });
+  const html = md2html(file, cliOptions);
   console.log(html);
 });
