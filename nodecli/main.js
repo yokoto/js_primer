1f30887e5175e95671a723f541b384210f74362f
yokoto <yo44ko55@gmail.com>
2020-07-23 17:57:48 +0900

    ファイルを読み込む

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,10 +1,23 @@
-console.log(process.argv);
-
-const myModule = require('./my-module');
-console.log(myModule.foo);
-
 const program = require('commander');
-program.parse(process.argv);
+const fs = require('fs');
 
+program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
+
+// 非同期形式
+fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
+  console.log(file);
+});
+
+// 同期形式
+// try {
+//   const file = fs.readFileSync(filePath);
+// } catch (err) {
+//   // error handling
+// }
+
