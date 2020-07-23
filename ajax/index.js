e6f82013c8c4f4c28d42c5c55a2a8964980e9886
yokoto <yo44ko55@gmail.com>
2020-07-23 15:54:03 +0900

    Promiseを活用する

--- ajax/index.js
+++ ajax/index.js
@@ -1,33 +1,40 @@
-const userId = 'js-primer-example';
+function main() {
+  fetchUserInfo('js-primer-example')
+    .then((userInfo) => createView(userInfo))
+    .then((view) => displayView(view))
+    .catch((error) => {
+      console.error(`エラーが発生しました（${error}）`);
+    });
+}
 
 function fetchUserInfo(userId) {
-  // fetch API
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
       console.log(response.status);
       if (!response.ok) {
-        console.error('エラーレスポンス', response);
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
       } else {
-        return response.json().then(userInfo => {
-          console.log(userInfo);
-
-          const view = escapeHTML`
-              <h4>${userInfo.name} (@${userInfo.login})</h4>
-              <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height='100'>
-              <dl>
-                <dt>Location</dt>
-                <dd>${userInfo.location}</dd>
-                <dt>Repositories</dt>
-                <dd>${userInfo.public_repos}</dd>
-              </dl>
-            `;
-          const result = document.getElementById('result');
-          result.innerHTML = view;
-        });
+        return response.json();
       }
-    }).catch(error => {
-      console.error(error);
     });
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height='100'>
+    <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+    </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
