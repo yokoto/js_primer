73643240d5b9e21c25837552b791150ad0cb8be2
yokoto <yo44ko55@gmail.com>
2020-07-24 22:19:01 +0900

    Todoアイテムの追加を実装する

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,19 @@
-console.log('App.js: loaded');
+import { element } from './view/html-util.js';
+
 export class App {
-  constructor() {
-    console.log('App initialized');
+  mount() {
+    const formElement = document.querySelector('#js-form');
+    const inputElement = document.querySelector('#js-form-input');
+    const containerElement = document.querySelector('#js-todo-list');
+    const todoItemCountElement = document.querySelector('#js-todo-count');
+    let todoItemCount = 0;
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      containerElement.appendChild(todoItemElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = '';
+    });
   }
 }
---
+++ todo/src/view/html-util.js
@@ -0,0 +1,31 @@
+export function escapeSpecialChars(str) {
+  return str
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#039;');
+}
+
+export function htmlToElement(html) {
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
+
+export function element(strings, ...values) {
+  const htmlString = strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === 'string') {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+  return htmlToElement(htmlString);
+}
+
+export function render(bodyElement, containerELement) {
+  containerELement.innerHTML = '';
+  containerELement.appendChild(bodyElement);
+}
