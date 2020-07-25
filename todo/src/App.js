0cb8df3a57f81bacddcb2aa5b56c84a957752e0d
yokoto <yo44ko55@gmail.com>
2020-07-25 17:02:30 +0900

    Todoアイテムの更新と削除を実装

--- todo/src/App.js
+++ todo/src/App.js
@@ -15,7 +15,28 @@ export class App {
       const todoListElement = element`<ul />`;
       const todoItems = this.todoListModel.getTodoItems();
       todoItems.forEach(item => {
-        const todoItemElement = element`<li>${item.title}</li>`;
+        const todoItemElement = item.completed
+          ? element`<li><input type='checkbox' class='checkbox' checked>
+            <s>${item.title}</s>
+            <button class='delete'></button>
+          </li>`
+          : element`<li><input type='checkbox' class='checkbox'>
+            ${item.title}
+            <button class='delete'>×</button>
+          </li>`;
+        const inputCheckboxElement = todoItemElement.querySelector('.checkbox');
+        inputCheckboxElement.addEventListener('change', () => {
+          this.todoListModel.updateTodo({
+            id: item.id,
+            completed: !item.completed
+          });
+        });
+        const deleteButtonElement = todoItemElement.querySelector('.delete');
+        deleteButtonElement.addEventListener('click', () => {
+          this.todoListModel.deleteTodo({
+            id: item.id
+          });
+        });
         todoListElement.appendChild(todoItemElement);
       });
       render(todoListElement, containerElement);
--- todo/src/model/TodoListModel.js
+++ todo/src/model/TodoListModel.js
@@ -26,4 +26,20 @@ export class TodoListModel extends EventEmitter {
     this.items.push(todoItem);
     this.emitChange();
   }
+
+  updateTodo({ id, completed }) {
+    const todoItem = this.items.find(todo => todo.id === id);
+    if (!todoItem) {
+      return;
+    }
+    todoItem.completed = completed;
+    this.emitChange();
+  }
+
+  deleteTodo({ id }) {
+    this.items = this.items.filter(todo => {
+      return todo.id !== id;
+    });
+    this.emitChange();
+  }
 }
